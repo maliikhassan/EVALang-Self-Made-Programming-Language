82c609ad300ed3e69a131002abb23a5f9490adc1
maliikhassan <maliik.hassan.009@gmail.com>
2025-01-10 16:59:23 +0500

    power functionality implemented

--- evalang.py
+++ evalang.py
@@ -632,12 +632,12 @@ global_symbol_table = SymbolTable()
 global_symbol_table.set("null", Number(0))
 
 def run(fn, text):
-	# Generate tokens
+	# Generating tokens
 	lexer = Lexer(fn, text)
 	tokens, error = lexer.make_tokens()
 	if error: return None, error
 	
-	# Generate AST
+	# Generating AST
 	parser = Parser(tokens)
 	ast = parser.parse()
 	if ast.error: return None, ast.error
