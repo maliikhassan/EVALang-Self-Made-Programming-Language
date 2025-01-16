e6851d702cc3f3dd193ae1e268aa8c5f2c0ec40d
maliikhassan <maliik.hassan.009@gmail.com>
2025-01-16 23:11:38 +0500

    code update and grammer adittion

--- shell.py
+++ shell.py
@@ -2,7 +2,7 @@ import evalang
 
 while True:
 	text = input('EvaLang > ')
-	result, error = basic.run('<stdin>', text)
+	result, error = evalang.run('<stdin>', text)
 
 	if error: print(error.as_string())
 	else: print(result)
